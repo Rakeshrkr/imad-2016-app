13b129c61d009bc3eb01cc5553319f4cbf5c9339
Rakesh Kumar Regar <jrakesh.iitb@gmail.com>
2016-09-18 22:06:54 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -1,15 +1,61 @@
 var express = require('express');
 var morgan = require('morgan');
 var path = require('path');
-
 var app = express();
 app.use(morgan('combined'));
 
+
+var aboutus={
+    title: "Abous Us | Rakesh" ,
+    heading: "Login Page" ,
+    date: "5th sept 2016" ,
+    content: `<div>
+        <p>This is the content of About us page. This is the content of About us page.This is the content of About us page. This is the content of About us page. This is the content of About us page.  </p>
+        <p>This is the content of About us page. This is the content of About us page.This is the content of About us page. This is the content of About us page. This is the content of About us page.  </p>
+        <p>This is the content of About us page. This is the content of About us page.This is the content of About us page. This is the content of About us page. This is the content of About us page.  </p>
+    </div> `
+};
+
+function createTamplete(data){
+    var title = data.title;
+    var heading =data.heading;
+    var date = data.date;
+    var content = data.content;
+    
+var htmltemplete=`
+<!Doctype html>
+<html>
+    <head>
+        <title>
+         ${title}
+        </title>
+        <link href="/ui/style.css" rel="stylesheet" />
+        <meta name="viewport" content="width=device-width,initial-scale=1"/>
+    </head>
+<body  class="container">
+    <div >
+    <div>
+        <a href="/">Home</a>
+    </div>
+    <hr>
+    <h2>A${heading}</h2>
+    <div>
+        ${date}
+    </div>
+        ${content}
+    </div>
+    
+</body>
+
+</html>
+`;
+return htmlTemplete ;
+}
 app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/aboutUs',function(req,res){
-  res.sendFile(path.join(__dirname, 'ui', 'aboutUs.html'));
+  res.send(crateTamplete(aboutus));
 });
 app.get('/Login',function(req,res){
   res.sendFile(path.join(__dirname, 'ui', 'Login.html'));
